41e9786ef1401374484ca6c0d36b1d3e32f44dd9
Alejandro <alesm91@hotmail.com>
2025-08-29 10:33:55 +0200

    ajouter enregistrement utilisateur et connexion bdd

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,15 @@
+import express from "express";
+import {register, verifyEmail, getByEmail, login, changeRole, logout } from "../controllers/userController.js";
+
+import { requireAuth, verifyToken } from '../middlewares/authMiddleware.js';
+
+const router = express.Router();
+
+router.post("/register", register);
+router.get('/verify/:token', verifyEmail);
+router.post('/getByEmail', verifyToken, getByEmail);
+router.post('/login', login);
+router.put('/changeRole', verifyToken, changeRole);
+router.get('/logout', requireAuth, logout);
+
+export default router;
